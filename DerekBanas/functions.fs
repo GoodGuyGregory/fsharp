e197aa912606f6a85a231d55eb6377b36c12d818
GoodGuyGregory <greg.witt625@gmail.com>
2019-12-29 17:32:47 -0500

    adds recursive notes

--- DerekBanas/functions.fs
+++ DerekBanas/functions.fs
@@ -7,10 +7,16 @@ let get_sum (x : int, y : int) : int = x + y
 
 
 // Factorial Functions with Recursion
-// let rec factorial x = 
-//         if X < 1 then 1
-//         else x * factorial (x - 1) 
+let rec factorial x = 
+        if x < 1 then 1
+        else x * factorial (x - 1) 
 
+// Recursive Function calls are done with (factorial 4);;
 
+    // Recursion Breakdown can be seen below:
+        // 1st: result = 4 * factorial(3) = 4 * 6 = 24
+        // 2nd: result = 3 * factorial(2) = 3 * 2 = 6
+        // 3rd: result = 2 * factorial(1) = 2 * 1 = 2
+    
 
 Console.ReadKey() |> ignore
