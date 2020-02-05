98d708740b35e889e4ebb72d1bfee41ae40deb7b
GoodGuyGregory <greg.witt625@gmail.com>
2020-02-05 18:00:58 -0500

    adds chapter 9

--- c311/Chapter 9/Chapter9Functions.fsx
+++ c311/Chapter 9/Chapter9Functions.fsx
@@ -2,3 +2,25 @@ let add x y = x + y
 
 // Anonymouse function
 let add2 = (fun x -> (fun y -> x + y))
+
+let rec sum L =
+    match L with
+    | [] -> 0
+    | h :: t -> h + (sum t)
+
+let rec tailSum a L =
+    let rec sum a L =
+        match L with
+        | [] -> a
+        | h :: t -> tailSum (h + a) t
+    sum 0 L
+
+let rec rdc L =
+    match L with
+    | h :: [] -> []
+    | h :: t -> h :: rdc t
+
+let rec tailRdc a L =
+    match L with
+    | h :: [] -> a
+    | h :: t -> tailRdc (h :: a) t
