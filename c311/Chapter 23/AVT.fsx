d6223a91b1e4dd0c7bfdeab10cbd6e40d4f63fc8
GoodGuyGregory <greg.witt625@gmail.com>
2020-03-09 18:42:01 -0400

    adds notes for language 3

--- c311/Chapter 23/AVT.fsx
+++ c311/Chapter 23/AVT.fsx
@@ -10,3 +10,10 @@ let rec valN exp =
     | Plus(e1, e2) -> valN (Plus(valN e1, valN e2))
     | Times(Const v1, Const v2) -> Const(v1, v2)
     | Times(e1, e2) -> valN (Timess(valN e1, valN e2))
+
+let x = 1 in f = (fun n)
+
+let f5 = (Apply(Var "f", Const 5))
+let x2 = (let ("x", Const 2, f5))
+let f = (let ("f", Fn ("n", Times (Var "n", Var "x")), x2))
+let x1 = (Let ( "x", Const 1, f))
